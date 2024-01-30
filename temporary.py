a25ba7c525e00e59af54e510e8a1a5b222a34758
austin-equifax <132520793+austin-equifax@users.noreply.github.com>
2024-01-30 09:51:58 -0500

    Update temporary.py
    Additional Queries

--- temporary.py
+++ temporary.py
@@ -1,5 +1,55 @@
 import json 
+import MySQLdb
+from datetime import datetime
 from ftplib import FTP 
+
+db_host = ""
+db_username = ""
+db_password = ""
+db_name = ""
+
+try:
+    conn = MySQLdb.connect(host=db_host, user=db_username, passwd=db_password, db=db_name)
+
+    # Create a cursor object
+    cursor = conn.cursor()
+
+    # Define two SQL queries to execute
+    query_dict = {
+        license_source: ""
+        
+        sanctions_source: ""
+        sanctions_products: ""
+        sanctions_tags: ""
+    
+        healthstats_data: ""
+        healthstats_products: ""
+        healthstats_tags: ""
+    }
+
+for key, val in query_dict.items():
+    # Execute the first SQL query
+    cursor.execute(val)
+    results = cursor.fetchall()
+
+    date_object = datetime.now(tz=timezone.utc)
+    date_string = "_" + date_object.strftime('%Y%m%d_%H:%M:%S')
+    query_name = key + date_string
+
+    # Output results of the first query to a text file
+    with open('{query_name}.csv'.format(query_name=query_name),'w') as file:
+        for row in results:
+            file.write(str(row) + '\n')
+
+print("Queries executed and results saved to text files.")
+
+except MySQLdb.Error as e:
+    print(f"Error: {e}")
+
+finally:
+    # Close the cursor and the connection
+    cursor.close()
+    conn.close()
 
 ftp_server = ""
 email = ""
