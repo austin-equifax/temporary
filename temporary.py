a4beb150aaff026d6ec827cc4c221efa73a07ebb
austin-equifax <132520793+austin-equifax@users.noreply.github.com>
2024-01-30 10:19:40 -0500

    Update temporary.py

--- temporary.py
+++ temporary.py
@@ -1,74 +1,93 @@
 import json 
 import MySQLdb
 from datetime import datetime
+import paramiko #2.7.2
 from ftplib import FTP 
 
-db_host = ""
-db_username = ""
-db_password = ""
-db_name = ""
 
-try:
-    conn = MySQLdb.connect(host=db_host, user=db_username, passwd=db_password, db=db_name)
+executed_query_list = []
 
-    # Create a cursor object
-    cursor = conn.cursor()
-
-    # Define two SQL queries to execute
-    query_dict = {
-        license_source: ""
-        
-        sanctions_source: ""
-        sanctions_products: ""
-        sanctions_tags: ""
+def populate_db():
+    db_host = ""
+    db_username = ""
+    db_password = ""
+    db_name = ""
     
-        healthstats_data: ""
-        healthstats_products: ""
-        healthstats_tags: ""
-    }
-
-    for key, val in query_dict.items():
-        # Execute the first SQL query
-        cursor.execute(val)
-        results = cursor.fetchall()
-    
-        date_object = datetime.now(tz=timezone.utc)
-        date_string = "_" + date_object.strftime('%Y%m%d_%H:%M:%S')
-        query_name = key + date_string
-    
-        # Output results of the first query to a text file
-        with open('{query_name}.csv'.format(query_name=query_name),'w') as file:
-            for row in results:
-                file.write(str(row) + '\n')
-    
-    print("Queries executed and results saved to text files.")
-
-except MySQLdb.Error as e:
-    print(f"Error: {e}")
-
-finally:
-    # Close the cursor and the connection
-    cursor.close()
-    conn.close()
-
-ftp_server = ""
-email = ""
-username = ""
-password = ""
-
-def lambda_handler(event, context):
-    status = None 
     try:
-        ftp = FTP(ftp_server)
-        ftp.login(user=username, passwd=password)
-        with open("source.txt", "rb") as source_file:
-            ftp.storbinary("STOR source.txt", source_file)
-
-        status = "passed"
-    except Exception as e:
-        status = e
+        conn = MySQLdb.connect(host=db_host, user=db_username, passwd=db_password, db=db_name)
     
-    return {
-        'statusCode': 200,
-        'body': json.dumps(status)
-    }
+        # Create a cursor object
+        cursor = conn.cursor()
+    
+        # Define two SQL queries to execute
+        query_dict = {
+            transfer_license_source: ""
+            
+            transfer_sanctions_source: ""
+            transfer_sanctions_products: ""
+            transfer_sanctions_tags: ""
+        
+            transfer_healthstats_data: ""
+            transfer_healthstats_products: ""
+            transfer_healthstats_tags: ""
+        }
+    
+        for key, val in query_dict.items():
+            # Execute the first SQL query
+            cursor.execute(val)
+            results = cursor.fetchall()
+        
+            date_object = datetime.now(tz=timezone.utc)
+            date_string = "_" + date_object.strftime('%Y%m%d_%H:%M:%S')
+            query_name = key + date_string
+        
+            # Output results of the first query to a text file
+            with open('{query_name}.csv'.format(query_name=query_name),'w') as file:
+                for row in results:
+                    file.write(str(row) + '\n')
+
+            executed_query_list.append(key)
+        print("Queries executed and results saved to text files.")
+    
+    except MySQLdb.Error as e:
+        print(f"Error: {e}")
+    
+    finally:
+        # Close the cursor and the connection
+        cursor.close()
+        conn.close()
+
+
+def transfer_files():
+    host = ""
+    port = 22 
+    username = ""
+    password = ""
+
+    local_path = "./"
+    remote_path = "workforce/WSAISPROB005/incoming
+
+    ssh_client = paramiko.SSHClient()
+
+    try:
+        
+        if len(executed_query_list) == 0:
+            raise ValueError("No executed queries found")
+            
+        ssh_client.connect(hostname=host, port=port, username=username, password=password)
+        sftp = ssh_client.open_sftp()
+    
+        # Transfer
+        for file_name in executed_query_list:
+            local_file = local_path + file_name
+            sftp.put(local_file, remote_path)
+            print(f"File {local_file} successfully transferred to {remote_path}")
+    
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    
+    finally:
+        # Close the SFTP session and SSH client
+        if 'sftp' in locals():
+            sftp.close()
+        ssh_client.close()
